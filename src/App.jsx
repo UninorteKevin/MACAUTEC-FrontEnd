e3cf286b6a861a4ea6ad066dd0a4301fa22d8589
UninorteKevin <kemartinez@uninorte.edu.co>
2021-12-02 15:47:13 -0500

    Primer commit y se sube proyecto para arquitectura FrontEnd

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <h3>Hola</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import './NavBar.css';
+import {Button, Navbar, NavDropdown, Container} from 'react-bootstrap';
+const NavBar = () => {
+    return(
+        <Navbar className = "nav-design" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">MacAutec Web</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className="justify-content-end ">
+                <Navbar.Brand href="#home">
+                    <img
+                        src="/profile_default.svg"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+ 
+export default NavBar;
