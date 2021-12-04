541a5b2638e756dc3028e512991a99cc2bbe9e84
JeimmyDeAguasSotelo <jsotelo@uninorte.edu.co>
2021-12-03 23:33:19 -0500

    agregado gestionando usuarios que sirve

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,40 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Sidebar from "./components/Sidebar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+//Components
+import UsuariosGestionar from "./components/usuarios-gestionar";
 
 function App() {
-  return (    
-    <div>      
-      <NavBar />
-      <Sidebar />
-      <h3>Hola</h3>
+  return (
+  <Router>    
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+        <Sidebar />
+      </header>
+
+      <Container>
+        <Row>
+          <Col md={12}>
+            <div className="wrapper">
+              <Routes>
+                <Route path="usuarios-gestionar" element={<UsuariosGestionar />} />
+              </Routes>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  );
+  </Router>);
 }
 
 export default App;
