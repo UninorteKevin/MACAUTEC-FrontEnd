f2db9695442dadb5867293140d1188eb73672070
JeimmyDeAguasSotelo <jsotelo@uninorte.edu.co>
2021-12-03 21:07:01 -0500

    agregue sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return (
-    <div>
+  return (    
+    <div>      
       <NavBar />
+      <Sidebar />
       <h3>Hola</h3>
     </div>
   );
